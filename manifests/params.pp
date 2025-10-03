206adb041cadffb6ac462a97ad3e4ca12a5269c8
Martin Meinhold <Martin.Meinhold@gmx.de>
2025-10-03 19:32:46 +0200

    Puppet 8 compatibility

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,36 +11,36 @@
 # Copyright 2014 Martin Meinhold, unless otherwise noted.
 #
 class duplicity_mysql::params {
-  $backup_dir = $::osfamily ? {
+  $backup_dir = $facts['os']['family'] ? {
     default => '/var/backups/mysql'
   }
 
   $dump_script_template = 'duplicity_mysql/usr/local/sbin/dump-mysql-database.sh.erb'
-  $dump_script_path = $::osfamily ? {
+  $dump_script_path = $facts['os']['family'] ? {
     default => '/usr/local/sbin/dump-mysql-database.sh'
   }
 
   $check_script_template = 'duplicity_mysql/usr/local/sbin/check-mysql-database.sh.erb'
-  $check_script_path = $::osfamily ? {
+  $check_script_path = $facts['os']['family'] ? {
     default => '/usr/local/sbin/check-mysql-database.sh'
   }
 
   $restore_script_template = 'duplicity_mysql/usr/local/sbin/restore-mysql-database.sh.erb'
-  $restore_script_path = $::osfamily ? {
+  $restore_script_path = $facts['os']['family'] ? {
     default => '/usr/local/sbin/restore-mysql-database.sh'
   }
 
-  $option_file = $::osfamily ? {
+  $option_file = $facts['os']['family'] ? {
     default => "${::root_home}/.my.cnf"
   }
 
-  $mysql_client_package_name = $::osfamily ? {
+  $mysql_client_package_name = $facts['os']['family'] ? {
     default => 'mysql-client'
   }
-  $grep_package_name = $::osfamily ? {
+  $grep_package_name = $facts['os']['family'] ? {
     default => 'grep'
   }
-  $gzip_package_name = $::osfamily ? {
+  $gzip_package_name = $facts['os']['family'] ? {
     default => 'gzip'
   }
 }
